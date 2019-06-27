c85d45a104b553754471faa638628282114d0e14
Adrian Popan <adrian.popan@gmail.com>
2019-06-26 22:30:24 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require "pry";
+
 class Shoe
+
+  BRANDS = []
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
@@ -12,3 +18,8 @@ class Shoe
   end
 
 end
+
+tennis_shoe = Shoe.new("Adidas")
+soccer_shoe = Shoe.new("Nike")
+running_shoe = Shoe.new("Nike")
+p Shoe::BRANDS
