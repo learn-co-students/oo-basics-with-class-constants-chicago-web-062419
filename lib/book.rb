2992ea7daaf9f9404630bd8a570fdb02746ecb25
github username <github email address>
2019-07-02 16:29:07 -0500

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+#1.Explicitly define the genre= method, to integrate our class constant into the method
+#2.Remove the attr_accessor for :genre since we no longer need to generate a reader AND a writer.
+#3.Add an attr_reader for :genre, since we still want Ruby to generate a reader for us.
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -9,5 +13,16 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+ 
+  # Explicitly define the genre= method, to integrate our class constant into the method
+  def genre=(genre)  # create the writer for genre and add the logic for the class CONSTANT
+    @genre = genre
+    GENRES << genre 
+  end
 
 end
+
+# hp = Book.new("harry potter")
+# p hp
+# hp.genre = "fantasy"
+# p hp
